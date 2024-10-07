77f435cdace5c9d1bc2615f79d963a7c449bdd07
Aaron <aaroncastro8392@gmail.com>
2024-10-07 18:15:15 -0500

    add symbol function

--- Tic-Tac-Toe/src/components/GameBoard.jsx
+++ Tic-Tac-Toe/src/components/GameBoard.jsx
@@ -8,17 +8,21 @@ const initialGameBoard = [
 export default function GameBoard() {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
     
-    function handleSelectSquare() {
-
+    function handleSelectSquare(row, col) {
+        setGameBoard((prevGameBoard)=> {
+            const updateGameBoard = [...prevGameBoard].map( rowBoard => [...rowBoard]);
+            updateGameBoard[row][col] = "X"
+            return updateGameBoard;
+        })
     }
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={()=> handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
