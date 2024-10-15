dfed8a56b80bc5c2bbf2c732c2a8bc664570b178
Aaron <aaroncastro8392@gmail.com>
2024-10-15 18:35:53 -0500

    add styles by CSS Modules

--- React Styles/src/components/AuthInputs.jsx
+++ React Styles/src/components/AuthInputs.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styles from './Inputs.module.css';
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -17,36 +18,48 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
-  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+  let labelStyleEmail = styles.label, inputStyleEmail = styles.input;
+  let labelStylePass = styles.label, inputStylePass = styles.input;
+
+  if(submitted && !enteredEmail.includes('@')){
+    labelStyleEmail += " " + styles.invalidLabel;
+    inputStyleEmail += ` ${styles.invalidInput}`;
+  }
+
+  if(submitted && enteredPassword.trim().length < 6){
+    labelStylePass += ` ${styles.invalidLabel}`;
+    inputStylePass += ` ${styles.invalidInput}`;
+  }
+
+
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div id="auth-inputs" className={styles.authInputs}>
+      <div className={styles.controls} >
         <p>
-          <label>Email</label>
+          <label className={labelStyleEmail}>Email</label>
           <input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            className={inputStyleEmail}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={labelStylePass}>Password</label>
           <input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            className={inputStylePass}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
         </p>
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className={styles.actions}>
+        <button type="button" className={styles.textButton}>
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className={styles.button} onClick={handleLogin}>Sign In</button>
       </div>
     </div>
   );
--- React Styles/src/components/Header.jsx
+++ React Styles/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import logo from '../assets/logo.png';
+import styles from "./Header.module.css"
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles["header"]}>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
