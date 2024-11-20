16940f2a79825535d457becc786432c9d5cfc89d
Aaron <aaroncastro8392@gmail.com>
2024-11-19 22:52:33 -0500

    Page structure

--- moviesWeb/src/App.jsx
+++ moviesWeb/src/App.jsx
@@ -1,7 +1,19 @@
 const App = () => {
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    function handleChange(e) {
+    }
+
     return (
-        <div className="App">
-            <p>Hello World</p>
+        <div className="app">
+            <h1>Movies Web</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Enter movie" onChange={handleChange} />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
