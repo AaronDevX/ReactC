2a9d0d05a101f2fdd530bc8eb5c5c580d42839ab
Aaron <aaroncastro8392@gmail.com>
2024-11-20 13:54:54 -0500

    migrate to async function

--- moviesWeb/src/services/getMovies.js
+++ moviesWeb/src/services/getMovies.js
@@ -1,15 +1,16 @@
 const API_KEY = "ce693dda";
 
-export default function getMovies({search}) {
-    return fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
-        .then((res) => res.json())
-        .then((data) => {
-            return data['Search'].map(movie => {
-                return {
-                    title: movie['Title'],
-                    year: movie['Year'],
-                    image: movie['Poster']
-                }
-            })
-        })
+export default async function getMovies({search}) {
+    if (search === '') return
+
+    const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+    const data = await response.json();
+
+    return data['Search'].map(movie => {
+        return {
+            title: movie['Title'],
+            year: movie['Year'],
+            image: movie['Poster']
+        }
+    })
 }
