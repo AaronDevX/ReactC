c41532745911c6173dfebc105fbe9cd82618b04c
Aaron <aaroncastro8392@gmail.com>
2024-12-11 18:36:02 -0500

    Add Error state

--- HttpRequests/src/components/AvailablePlaces.jsx
+++ HttpRequests/src/components/AvailablePlaces.jsx
@@ -1,20 +1,33 @@
-import Places from './Places.jsx';
 import {useEffect, useState} from "react";
+import Places from './Places.jsx';
+import ErrorMessage from "./ErorMessage.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [loading, setLoading] = useState(true);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('http://localhost:3000/places');
-      const data = await response.json();
-      setAvailablePlaces(data.places);
-      setLoading(false);
-    }
+      try{
+        const response = await fetch('http://localhost:3000/placess');
+        const data = await response.json();
 
+        if(!response.ok){
+          throw new Error("Something went wrong");
+        }
+        setAvailablePlaces(data.places);
+        setLoading(false);
+      }catch(error){
+        setError({ message: error.message });
+      }
+    }
     fetchData();
   }, [])
+
+  if(error){
+    return (<ErrorMessage title={"Error"} message={error.message} />)
+  }
 
   return (
     <Places
