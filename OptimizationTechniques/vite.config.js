8ac8d1a6fb4f55dc7359306e1c19bbb95c172f0a
Aaron <aaroncastro8392@gmail.com>
2024-12-10 21:22:39 -0500

    million js

--- OptimizationTechniques/vite.config.js
+++ OptimizationTechniques/vite.config.js
@@ -1,6 +1,7 @@
-import react from '@vitejs/plugin-react'
-import {defineConfig} from 'vite'
+import MillionLint from "@million/lint";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [react()]
-})
+    plugins: [MillionLint.vite(), react()],
+});
