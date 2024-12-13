f7c3f730714c9f8adf9a5b10830192672cda5587
Aaron <aaroncastro8392@gmail.com>
2024-12-13 11:10:50 -0500

    manage form with state

--- Forms/src/components/Login.jsx
+++ Forms/src/components/Login.jsx
@@ -1,17 +1,33 @@
+import {useState} from "react";
+
 export default function Login() {
-  return (
-    <form>
+    const [formData, setFormData] = useState({email: '', password: ''});
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formData);
+    }
+
+    function handleChange(identifier, value) {
+        setFormData(prevData => ({
+            ...prevData,
+            [identifier]: value
+        }));
+    }
+
+    return (
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email"/>
+          <input id="email" type="email" name="email" onChange={(e) => handleChange('email', e.target.value)}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password"/>
+          <input id="password" type="password" name="password" onChange={(e) => handleChange('password', e.target.value)}/>
         </div>
       </div>
 
